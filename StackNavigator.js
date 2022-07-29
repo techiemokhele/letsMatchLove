5fc266f908370a1438ca8155dcac32114c333f4b
techiemokhele <neomokhele23@gmail.com>
2022-07-29 16:35:42 +0200

    auth function set-up v1.0

--- StackNavigator.js
+++ StackNavigator.js
@@ -4,16 +4,24 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 //screen components
 import HomeScreen from "./screens/HomeScreen";
 import MingleScreen from "./screens/MingleScreen";
+import SignInScreen from "./screens/Auth/SignInScreen";
 
+//create navigator stack
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
-  //create navigator stack
+  const user = false;
 
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Mingle" component={MingleScreen} />
+      {user ? (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Mingle" component={MingleScreen} />
+        </>
+      ) : (
+        <Stack.Screen name="Sign-in" component={SignInScreen} />
+      )}
     </Stack.Navigator>
   );
 };
